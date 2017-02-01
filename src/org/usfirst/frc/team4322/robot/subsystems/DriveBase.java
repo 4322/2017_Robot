ac43864b366a5abc1a3cbba4ffabaa2429a2afde
Nicolas Machado <nxmq08@gmail.com>
2017-01-31 20:11:22 -0800

    Added master and slave driveBase talons, started work on autonomous

--- src/org/usfirst/frc/team4322/robot/subsystems/DriveBase.java
+++ src/org/usfirst/frc/team4322/robot/subsystems/DriveBase.java
@@ -4,6 +4,7 @@ import org.usfirst.frc.team4322.robot.RobotMap;
 import org.usfirst.frc.team4322.robot.commands.DriveBase_DriveManual;
 
 import com.ctre.CANTalon;
+import com.ctre.CANTalon.TalonControlMode;
 import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.wpilibj.I2C.Port;
@@ -14,14 +15,20 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class DriveBase extends Subsystem
 {
 
-    private CANTalon left,right;
+    private CANTalon leftMaster,leftSlave,rightMaster,rightSlave;
     private AHRS navx;
     private RobotDrive drive;
     
     public DriveBase()
     {
-        left = new CANTalon(RobotMap.DRIVEBASE_MOTORCONTROLLER_LEFT_MASTER_ADDR);
-        right = new CANTalon(RobotMap.DRIVEBASE_MOTORCONTROLLER_RIGHT_MASTER_ADDR);
+        leftMaster = new CANTalon(RobotMap.DRIVEBASE_MOTORCONTROLLER_LEFT_MASTER_ADDR);
+        leftMaster.changeControlMode(TalonControlMode.Voltage);
+        rightMaster = new CANTalon(RobotMap.DRIVEBASE_MOTORCONTROLLER_RIGHT_MASTER_ADDR);
+        rightMaster.changeControlMode(TalonControlMode.Voltage);
+        leftSlave = new CANTalon(RobotMap.DRIVEBASE_MOTORCONTROLLER_LEFT_SLAVE_ADDR);
+        leftSlave.changeControlMode(TalonControlMode.Follower);
+        rightSlave = new CANTalon(RobotMap.DRIVEBASE_MOTORCONTROLLER_RIGHT_SLAVE_ADDR);
+        rightSlave.changeControlMode(TalonControlMode.Follower);
         drive = new RobotDrive(left,right);
         navx = new AHRS(Port.kMXP);
     }
