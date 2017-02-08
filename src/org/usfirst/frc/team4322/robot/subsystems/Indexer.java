e66b6f60af43eff5745119ad998cf4791a6ce9a6
Auto-Committer <software@frc4322.com>
2017-02-07 21:04:28 -0800

    [ant-git-tasks] Auto-commit

--- src/org/usfirst/frc/team4322/robot/subsystems/Indexer.java
+++ src/org/usfirst/frc/team4322/robot/subsystems/Indexer.java
@@ -22,7 +22,7 @@ public class Indexer extends Subsystem
         // Let's run closed-loop velocity control mode
         indexer.changeControlMode(TalonControlMode.Position);
         // Our encoder generates 4096 ticks per rev
-        indexer.configEncoderCodesPerRev(4096);
+        indexer.configEncoderCodesPerRev(8192);
         // Set our starting PID Control Values (P, I, D, FF, IZ, RR, Profile)
         indexer.setPID(RobotMap.INDEXER_P, RobotMap.INDEXER_I , RobotMap.INDEXER_D, RobotMap.INDEXER_F, RobotMap.INDEXER_IZ, RobotMap.INDEXER_R, 0);
     }
