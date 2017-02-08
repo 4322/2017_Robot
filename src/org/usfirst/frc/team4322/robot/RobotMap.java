a6163f6da02c6f87018c11d092152bf052148e4b
Auto-Committer <software@frc4322.com>
2017-02-07 21:33:33 -0800

    [ant-git-tasks] Auto-commit

--- src/org/usfirst/frc/team4322/robot/RobotMap.java
+++ src/org/usfirst/frc/team4322/robot/RobotMap.java
@@ -69,7 +69,7 @@ public class RobotMap
     // ** INDEXER MAP VALUES **
     // ************************
     @DashboardInputField(field="Indexer Target RPM: ")
-    public static double INDEXER_POS = 97.302;
+    public static double INDEXER_POS = 197.302;
     @DashboardInputField(field="Indexer P: ")
     public static double INDEXER_P = 0.10;
     @DashboardInputField(field="Indexer I: ")
