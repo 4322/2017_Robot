aa6c133e522dc07c525c69326dd9153a4a63e0b8
Auto-Committer <software@frc4322.com>
2017-03-02 21:15:59 -0800

    [ant-git-tasks] Auto-commit

--- src/org/usfirst/frc/team4322/robot/commands/GearGrabber_HoldGear.java
+++ src/org/usfirst/frc/team4322/robot/commands/GearGrabber_HoldGear.java
@@ -15,7 +15,7 @@ public class GearGrabber_HoldGear extends Command {
     }
     @Override
     protected void execute() {
-        Robot.gearGrabber.set(-0.2);
+        Robot.gearGrabber.set(-0.5);
     }
 
     @Override
