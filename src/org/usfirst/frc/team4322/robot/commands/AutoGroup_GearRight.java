1e7fbdbc45a719c1dfede9ff575b030884bf4b44
Auto-Committer <software@frc4322.com>
2017-03-06 15:12:31 -0800

    [ant-git-tasks] Auto-commit

--- src/org/usfirst/frc/team4322/robot/commands/AutoGroup_GearRight.java
+++ src/org/usfirst/frc/team4322/robot/commands/AutoGroup_GearRight.java
@@ -10,7 +10,9 @@ public class AutoGroup_GearRight extends CommandGroup
 {
     public AutoGroup_GearRight()
     {
-        addParallel(new DriveBase_DriveDistance(-90)); //Drive up to peg
+        addSequential(new DriveBase_DriveDistance(-90)); //Drive a certain distance
+        addSequential(new DriveBase_Turn(30,.25)); //Turn 30 degrees
+        addParallel(new DriveBase_DriveDistance(-30)); //Drive up to peg
         addSequential(new DriveBase_VisionTurn(() -> {
             return SmartDashboard.getNumber("Vision Center X: ", 0.5) - .5;
         }));
--- src/org/usfirst/frc/team4322/robot/commands/AutoGroup_Hopper.java
+++ src/org/usfirst/frc/team4322/robot/commands/AutoGroup_Hopper.java
@@ -10,15 +10,18 @@ public class AutoGroup_Hopper extends CommandGroup
 {
 	public AutoGroup_Hopper()
 	{
-		addParallel(new DriveBase_DriveDistance(-90)); //Drive up to peg
+		addParallel(new DriveBase_DriveDistance(-82)); //Drive up to peg
 		addSequential(new DriveBase_VisionTurn(() -> {
 			return SmartDashboard.getNumber("Vision Center X: ", 0.5) - .5;
 		}));
 		addSequential(new GearGrabber_EjectGear()); //Put gear on peg
 		addSequential(new DriveBase_DriveDistance(6)); //Back up a bit
-		addSequential(new DriveBase_Turn(45,0.25));
+		addSequential(new DriveBase_Turn(90,0.25)); //Turn 90 degrees
+		addParallel(new HopperFlopper_LoadHopper()); //Ready for hopper
+		addSequential(new DriveBase_DriveDistance(27/2)); //Drive up to hopper
+		addSequential(new Vision_RunBoiler());
 		addParallel(new Shooter_Spin());
-		addParallel(new Group_StartIndex());
-		addSequential(new DriveBase_DriveDistance(100.0));
+		addParallel(new Group_StartIndex()); //start shooting
+
 	}
 }
