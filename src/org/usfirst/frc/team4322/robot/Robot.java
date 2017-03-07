aa6def16810b4590df71c3c4dee65f0f324c2daa
Auto-Committer <software@frc4322.com>
2017-03-06 20:07:37 -0800

    [ant-git-tasks] Auto-commit

--- src/org/usfirst/frc/team4322/robot/Robot.java
+++ src/org/usfirst/frc/team4322/robot/Robot.java
@@ -140,6 +140,7 @@ public class Robot extends IterativeRobot
     public void autonomousPeriodic()
     {
         Scheduler.getInstance().run();
+        SmartDashboard.putNumber("Drivebase Encoder Value",Robot.driveBase.getDist());
     }
 
     public void teleopInit()
--- src/org/usfirst/frc/team4322/robot/commands/AutoGroup_ReachBaseline.java
+++ src/org/usfirst/frc/team4322/robot/commands/AutoGroup_ReachBaseline.java
@@ -9,6 +9,6 @@ public class AutoGroup_ReachBaseline extends CommandGroup
 {
     public AutoGroup_ReachBaseline()
     {
-        addSequential(new DriveBase_DriveDistance(20));
+        addSequential(new DriveBase_DriveDistance(-20));
     }
 }
