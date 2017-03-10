467cfc98ffda8618def56cf6a4e1adf79046a3ac
Auto-Committer <software@frc4322.com>
2017-03-10 09:17:27 -0800

    [ant-git-tasks] Auto-commit

--- src/org/usfirst/frc/team4322/robot/commands/AutoGroup_ShootStupid.java
+++ src/org/usfirst/frc/team4322/robot/commands/AutoGroup_ShootStupid.java
@@ -12,6 +12,6 @@ public class AutoGroup_ShootStupid extends CommandGroup
         addSequential(new Shooter_Spin(),2);
         addSequential(new Group_StartIndex(),8);
 	    addParallel(new Group_StopIndex(),.1);
-	    addSequential(new DriveBase_StupidDriveDistance(400));
+	    addSequential(new DriveBase_StupidFMSDrive(400));
     }
 }
