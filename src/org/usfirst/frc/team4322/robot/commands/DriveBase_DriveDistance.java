1afe2d5009f637f1d2d825d89f5a912200d89b5d
Auto-Committer <software@frc4322.com>
2017-03-18 12:49:46 -0700

    [ant-git-tasks] Auto-commit

--- src/org/usfirst/frc/team4322/robot/commands/DriveBase_DriveDistance.java
+++ src/org/usfirst/frc/team4322/robot/commands/DriveBase_DriveDistance.java
@@ -55,7 +55,7 @@ public class DriveBase_DriveDistance extends Command
         {
             counter=0;
 
-            Robot.driveBase.drive((cur<dist) ? .7 :  -.7, -Robot.driveBase.getAngle() * RobotMap.DRIVEBASE_AIM_P);
+            Robot.driveBase.drive((cur<dist) ?  Math.min(.7,RobotMap.DRIVEBASE_DRIVE_P*(cur-dist)+RobotMap.DRIVEBASE_DRIVE_D*last) :  Math.max(-.7,RobotMap.DRIVEBASE_DRIVE_P*(cur-dist)+RobotMap.DRIVEBASE_DRIVE_D*last), -Robot.driveBase.getAngle() * RobotMap.DRIVEBASE_AIM_P);
         }
     }
 
