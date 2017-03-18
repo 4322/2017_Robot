e18d6acab752cd95b110b8f6f9880bbddf1246a2
Auto-Committer <software@frc4322.com>
2017-03-18 15:23:47 -0700

    [ant-git-tasks] Auto-commit

--- src/org/usfirst/frc/team4322/robot/RobotMap.java
+++ src/org/usfirst/frc/team4322/robot/RobotMap.java
@@ -61,13 +61,13 @@ public class RobotMap
     // ** DRIVEBASE AIMING MAP VALUES **
     // *********************************
     @DashboardInputField(field="Drivebase Aim P: ")
-    public static double DRIVEBASE_AIM_P = .00115;
+    public static double DRIVEBASE_AIM_P = .001175;
     @DashboardInputField(field="Drivebase Aim I: ")
-    public static double DRIVEBASE_AIM_I = 0.00011;
+    public static double DRIVEBASE_AIM_I = 0.0001;
     @DashboardInputField(field="Drivebase Aim IZone: ")
     public static double DRIVEBASE_AIM_IZONE = 10;
     @DashboardInputField(field="Drivebase Aim D: ")
-    public static double DRIVEBASE_AIM_D = 0.0002125;
+    public static double DRIVEBASE_AIM_D = 0.000325;
     // *********************************
     // ** DRIVEBASE DRIVING MAP VALUES **
     // *********************************
