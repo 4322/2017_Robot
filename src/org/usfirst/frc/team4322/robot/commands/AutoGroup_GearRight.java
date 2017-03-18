d2665320175b2d1ec1b584d70a8196e76fbd79f7
Auto-Committer <software@frc4322.com>
2017-03-18 11:58:14 -0700

    [ant-git-tasks] Auto-commit

--- src/org/usfirst/frc/team4322/robot/commands/AutoGroup_GearRight.java
+++ src/org/usfirst/frc/team4322/robot/commands/AutoGroup_GearRight.java
@@ -11,8 +11,8 @@ public class AutoGroup_GearRight extends CommandGroup
     public AutoGroup_GearRight()
     {
         addSequential(new DriveBase_DriveDistance(90)); //Drive a certain distance
-        addSequential(new DriveBase_Turn(30)); //Turn 30 degrees
-        addParallel(new DriveBase_DriveDistance(30)); //Drive up to peg
+        addSequential(new DriveBase_Turn(60)); //Turn 30 degrees
+        addParallel(new DriveBase_DriveDistance(12)); //Drive up to peg
 //        addSequential(new DriveBase_VisionTurn(() -> {
 //            return SmartDashboard.getNumber("Vision Center X: ", 0.5) - .5;
 //        }));
