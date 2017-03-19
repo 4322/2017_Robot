2fda07b571e8c58ede61ec0d9fd1939b494e5a02
Auto-Committer <software@frc4322.com>
2017-03-19 14:01:55 -0700

    [ant-git-tasks] Auto-commit

--- src/org/usfirst/frc/team4322/robot/commands/AutoGroup_GearBoiler.java
+++ src/org/usfirst/frc/team4322/robot/commands/AutoGroup_GearBoiler.java
@@ -11,18 +11,19 @@ public class AutoGroup_GearBoiler extends CommandGroup
 {
     public AutoGroup_GearBoiler()
     {
-        addSequential(new DriveBase_DriveDistance(90, true)); //Drive a certain distance
-        addSequential(new DriveBase_Turn(60)); //Turn 30 degrees
-        addSequential(new DriveBase_DriveDistance(6, false)); //Drive up to peg
+//        addSequential(new DriveBase_DriveDistance(90, true)); //Drive a certain distance
+//        addSequential(new DriveBase_Turn(60)); //Turn 30 degrees
+//        addSequential(new DriveBase_DriveDistance(6, false)); //Drive up to peg
+
 //        addSequential(new DriveBase_VisionTurn(() -> {
 //            return SmartDashboard.getNumber("Vision Center X: ", 0.5) - .5;
 //        }));
-//        addParallel(new GearGrabber_EjectGear()); //Put gear on peg
-//        addSequential(new Command_Delay(500));
-//        addSequential(new DriveBase_DriveDistance(6)); //Back up a bit
 
-        addSequential(new DriveBase_DriveDistance(-16,false));
-        addSequential(new DriveBase_Turn(-60));
-        addSequential(new DriveBase_DriveDistance(-80,true));
+
+        addSequential(new DriveBase_DriveDistance(-16,false)); // Back up a bit
+        addSequential(new DriveBase_Turn(-60)); // Straighten out
+        addSequential(new DriveBase_DriveDistance(90,true)); // Drive to line
+        addSequential(new DriveBase_Turn(45)); // turn some angle
+        addSequential(new DriveBase_DriveDistance(200)); // DRIVE ALL THE WAY
     }
 }
