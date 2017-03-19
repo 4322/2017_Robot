4c0bb20e47eefb6e9746315d9281656e0337a3ab
Auto-Committer <software@frc4322.com>
2017-03-19 12:36:51 -0700

    [ant-git-tasks] Auto-commit

--- src/org/usfirst/frc/team4322/robot/commands/AutoGroup_DoNothing.java
+++ src/org/usfirst/frc/team4322/robot/commands/AutoGroup_DoNothing.java
@@ -9,6 +9,6 @@ public class AutoGroup_DoNothing extends CommandGroup
 {
     public AutoGroup_DoNothing()
     {
-        addSequential(new DriveBase_Turn(60));
+        addSequential(new DriveBase_DriveExperiment(0,0,60, true));
     }
 }
