99fac309739799e791e4041bc8ce0a6bae25056b
Auto-Committer <software@frc4322.com>
2017-03-28 20:25:50 -0700

    [ant-git-tasks] Auto-commit

--- src/org/usfirst/frc/team4322/robot/commands/Group_EjectGear.java
+++ src/org/usfirst/frc/team4322/robot/commands/Group_EjectGear.java
@@ -8,9 +8,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class Group_EjectGear extends CommandGroup {
     public Group_EjectGear() {
         addParallel(new GearGrabber_EjectGear());
-        addParallel(new GearPivot_Lower());
-        addSequential(new Command_Delay(250));
-        addParallel(new DriveBase_StupidDriveDistance(30,true));
+        addSequential(new GearPivot_Lower());
         addSequential(new Group_RaisePivot());
     }
 }
