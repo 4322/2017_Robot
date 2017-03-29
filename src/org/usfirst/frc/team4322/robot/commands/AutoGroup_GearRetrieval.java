d11a6ac7b17bf63221bc87f0718a99fd38ef23a5
Auto-Committer <software@frc4322.com>
2017-03-29 16:29:37 -0700

    [ant-git-tasks] Auto-commit

--- src/org/usfirst/frc/team4322/robot/commands/AutoGroup_GearRetrieval.java
+++ src/org/usfirst/frc/team4322/robot/commands/AutoGroup_GearRetrieval.java
@@ -13,11 +13,11 @@ public class AutoGroup_GearRetrieval extends CommandGroup
     {
         addSequential(new Group_HomePivot());
         addSequential(new DriveBase_DriveDistance(86.5, true,0.6,true,0.3125)); //Drive a certain distance
-        addSequential(new DriveBase_FMSTurn(60)); //Turn 30 degrees
+        addSequential(new DriveBase_FMSTurn(-60)); //Turn 30 degrees
         addSequential(new DriveBase_DriveDistance(36, false)); //Drive up to peg
         addSequential(new Group_EjectGear()); //Put gear on peg
         addSequential(new DriveBase_DriveDistance(-16,false, 0.8, false)); // Back up a bit
-        addSequential(new DriveBase_FMSTurn(-60)); // Straighten out
+        addSequential(new DriveBase_FMSTurn(60)); // Straighten out
         addSequential(new DriveBase_DriveDistance(54,true, 0.8,false)); // DRIVE ALL THE WAY
 
     }
