ef33f75806a632c65fc21c16ba93d7447e56599c
Auto-Committer <software@frc4322.com>
2017-03-28 21:26:27 -0700

    [ant-git-tasks] Auto-commit

--- src/org/usfirst/frc/team4322/robot/commands/AutoGroup_GearMiddle.java
+++ src/org/usfirst/frc/team4322/robot/commands/AutoGroup_GearMiddle.java
@@ -11,12 +11,8 @@ public class AutoGroup_GearMiddle extends CommandGroup
 {
     public AutoGroup_GearMiddle()
     {
-        addSequential(new DriveBase_DriveDistance(57,false,true,true));
-        addParallel(new GearGrabber_EjectGear());
-        addParallel(new GearPivot_Lower(),1);
-        addSequential(new DriveBase_StupidDriveDistance(30,true));
-        addSequential(new Group_RaisePivot());
         addSequential(new Group_HomePivot());
-
+        addSequential(new DriveBase_DriveDistance(54,false,false,false));
+        addSequential(new Group_EjectGear());
     }
 }
