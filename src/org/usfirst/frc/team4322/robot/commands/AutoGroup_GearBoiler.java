9a54456e81ff3d4db06f2f23573ac4eaeee354b7
Auto-Committer <software@frc4322.com>
2017-03-29 17:50:04 -0700

    [ant-git-tasks] Auto-commit

--- src/org/usfirst/frc/team4322/robot/commands/AutoGroup_GearBoiler.java
+++ src/org/usfirst/frc/team4322/robot/commands/AutoGroup_GearBoiler.java
@@ -14,17 +14,18 @@ public class AutoGroup_GearBoiler extends CommandGroup
         addSequential(new Group_HomePivot());
         addSequential(new DriveBase_DriveDistance(90.5, true,0.6,true,0.31)); //Drive a certain distance
         addSequential(new DriveBase_FMSTurn(60)); //Turn 30 degrees
-        addSequential(new DriveBase_DriveDistance(30, false,0.85,false)); //Drive up to peg
+//        addSequential(new DriveBase_DriveDistance(30, false,0.85,false)); //Drive up to peg
 //        addSequential(new DriveBase_DriveExperiment(90,6,60,true));
-        addSequential(new Group_EjectGear());
+//        addSequential(new Group_EjectGear());
 //        addSequential(new DriveBase_VisionTurn(() -> {
 //            return SmartDashboard.getNumber("Vision Center X: ", 0.5) - .5;
 //        }));
 
-        addSequential(new DriveBase_DriveDistance(-30,false, 0.8, false)); // Back up a bit
-        addSequential(new DriveBase_FMSTurn(-60)); // Straighten out
+//        addSequential(new DriveBase_DriveDistance(-30,false, 0.8, false)); // Back up a bit
+//        addSequential(new DriveBase_FMSTurn(-60)); // Straighten out
+//
+//        addSequential(new DriveBase_DriveDistance(54,true, 0.8,false,0.54)); // Drive to line
 
-        addSequential(new DriveBase_DriveDistance(54,true, 0.8,false,0.54)); // Drive to line
 //        addSequential(new DriveBase_FMSTurn(45)); // turn some angle
 //        addSequential(new DriveBase_DriveDistance(200,true,false, false)); // DRIVE ALL THE WAY
 //        addSequential(new DriveBase_FMSTurn(-45)); // face the retrieval zone
