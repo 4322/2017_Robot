652a337d336f37989b534638f4cc340884b9f61d
Auto-Committer <software@frc4322.com>
2017-03-30 14:03:42 -0700

    [ant-git-tasks] Auto-commit

--- src/org/usfirst/frc/team4322/robot/commands/GearGrabber_HoldGear.java
+++ src/org/usfirst/frc/team4322/robot/commands/GearGrabber_HoldGear.java
@@ -18,7 +18,7 @@ public class GearGrabber_HoldGear extends Command
     @Override
     protected void execute()
     {
-        Robot.gearGrabber.set(Relay.Value.kOff);
+        Robot.gearGrabber.set(0);
     }
 
     @Override
--- src/org/usfirst/frc/team4322/robot/commands/GearGrabber_PrepareForGear.java
+++ src/org/usfirst/frc/team4322/robot/commands/GearGrabber_PrepareForGear.java
@@ -15,7 +15,7 @@ public class GearGrabber_PrepareForGear extends Command {
     }
     @Override
     protected void execute() {
-        Robot.gearGrabber.set(Relay.Value.kReverse);
+        Robot.gearGrabber.set(1);
     }
 
     @Override
