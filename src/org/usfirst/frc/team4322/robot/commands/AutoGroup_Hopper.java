3ad13271e0ac939731ee591602c9ae83b5aab696
Auto-Committer <software@frc4322.com>
2017-03-29 19:34:20 -0700

    [ant-git-tasks] Auto-commit

--- src/org/usfirst/frc/team4322/robot/commands/AutoGroup_Hopper.java
+++ src/org/usfirst/frc/team4322/robot/commands/AutoGroup_Hopper.java
@@ -12,9 +12,9 @@ public class AutoGroup_Hopper extends CommandGroup
 	public AutoGroup_Hopper()
 	{
 		addSequential(new Group_HomePivot());
-		addSequential(new DriveBase_DriveDistance(-78)); //Drive a distance
+		addSequential(new DriveBase_DriveDistance(-78,true,0.6,true,0.31)); //Drive a distance
 		addSequential(new DriveBase_Turn(90)); //Turn 90 degrees
-		addSequential(new DriveBase_DriveDistance(-60)); //Drive up to hopper
+		addSequential(new DriveBase_DriveDistance(-60,true,0.6,true,0.31)); //Drive up to hopper
 //		addSequential(new Vision_RunBoiler());
 //		addParallel(new Shooter_Spin());
 //		addSequential(new Group_StartIndex()); //start shooting
