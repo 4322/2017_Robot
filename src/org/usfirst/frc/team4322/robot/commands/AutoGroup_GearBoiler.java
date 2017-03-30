ca8522cb5a837637729d1efaa92b4d5a562f39d5
Auto-Committer <software@frc4322.com>
2017-03-29 17:27:06 -0700

    [ant-git-tasks] Auto-commit

--- src/org/usfirst/frc/team4322/robot/commands/AutoGroup_GearBoiler.java
+++ src/org/usfirst/frc/team4322/robot/commands/AutoGroup_GearBoiler.java
@@ -14,7 +14,7 @@ public class AutoGroup_GearBoiler extends CommandGroup
         addSequential(new Group_HomePivot());
         addSequential(new DriveBase_DriveDistance(86.5, true,0.6,true,0.31)); //Drive a certain distance
         addSequential(new DriveBase_FMSTurn(60)); //Turn 30 degrees
-        addSequential(new DriveBase_DriveDistance(50, false,0.85,false)); //Drive up to peg
+        addSequential(new DriveBase_DriveDistance(40, false,0.85,false)); //Drive up to peg
 //        addSequential(new DriveBase_DriveExperiment(90,6,60,true));
         addSequential(new Group_EjectGear());
 //        addSequential(new DriveBase_VisionTurn(() -> {
